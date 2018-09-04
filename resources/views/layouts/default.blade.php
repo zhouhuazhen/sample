596fab7775a6d899c7613da8c6200185974a1856
zhz <382938018@qq.com>
2018-09-04 02:30:13 +0000

    静态页面的添加

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>@yield('title','Sample') - Laravel 新手入门教程</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	@yield('content')
+</body>
+</html>
